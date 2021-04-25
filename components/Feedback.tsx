783b20f42e8b3b196724efa83eec82ac6c3fe441
Ahmed Elsenousi <asenousy@gmail.com>
2021-04-25 10:47:24 +0100

    less dim background

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -64,7 +64,7 @@ const styles = StyleSheet.create(
       width: "100%",
       height: "100%",
       backgroundColor: colors.feedback.shadow,
-      opacity: 0.8,
+      opacity: 0.5,
     },
     modal: {
       borderRadius: 10,
